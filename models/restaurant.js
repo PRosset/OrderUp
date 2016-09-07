e2253a3af58bfe398ab0ab4c461e91db700a7cee
Leslie Hochsztein <lhochsz@gmail.com>
2016-09-07 13:52:06 -0400

    Started work on seeds file.

--- models/restaurant.js
+++ models/restaurant.js
@@ -2,8 +2,8 @@ var mongoose = require('mongoose');
 
 var RestaurantSchema = new mongoose.Schema({
     title:        { type: String,  required: true },
-    address:      { type: String,  required: true },
-    description:  { type: String,  required: true },
+    // address:      { type: String,  required: true },
+    // description:  { type: String,  required: true },
     menu:       [ { type: mongoose.Schema.Types.ObjectId, ref: 'Item'  } ],
     // orders:     [ { type: mongoose.Schema.Types.ObjectId, ref: 'Order' } ]
 });
--- seeds.js
+++ seeds.js
@@ -2,9 +2,11 @@ var mongoose = require('mongoose');
 mongoose.Promise = require('bluebird');
 
 var User = require('./models/user');
-var Todo = require('./models/todo');
+var Item = require('./models/item');
+var Order = require('./models/order');
+var Restaurant = require('./models/restaurant');
 
-mongoose.connect('mongodb://localhost/todos');
+mongoose.connect('mongodb://localhost/orderup');
 
 // our script will not exit until we have disconnected from the db.
 function quit() {
@@ -19,38 +21,64 @@ function handleError(err) {
   return err;
 }
 
-console.log('removing old todos...');
-Todo.remove({})
+console.log('removing old items...');
+Item.remove({})
 .then(function() {
   console.log('removing old users...');
-  return User.remove({});
+  return User.remove({})
+})
+.then(function() {
+  console.log('removing old restaurants...');
+  return Restaurant.remove({})
+})
+.then(function() {
+  console.log('removing old orders...');
+  return Order.remove({});
 })
 .then(function() {
   console.log('creating new users');
-  let joe = new User();
-  joe.local = { email: 'joe@ga.co', password: joe.encrypt('test1234') };
-  let sue = new User();
-  sue.local = { email: 'sue@ga.co', password: sue.encrypt('test1234') };
-  return [User.create(joe), User.create(sue)];
+  let adele = new User();
+  adele = {
+    local:  { email: 'hello@itsme.com', password: adele.encrypt('123') },
+    firstName: 'Adele',
+    lastName: 'Adkins',
+    restaurants: []
+  };
+
+  let andre = new User();
+  andre = {
+    local: { email: 'hey@ya.com', password: andre.encrypt('123') },
+    firstName: 'Andre',
+    lastName: '3000',
+    restaurants: []
+  };
+  console.log(adele);
+  console.log(andre);
+  return [User.create(adele), User.create(andre)];
 })
-.spread(function(joe, sue) {
-  console.log('creating some new todos...');
-  var groceries    = new Todo({ title: 'groceries',       completed: false, user: joe._id });
-  var feedTheCat   = new Todo({ title: 'feed the cat',    completed: true,  user: joe._id });
-  var learnAngular = new Todo({ title: 'Learn AngularJS', completed: true,  user: sue._id });
-  var updateResume = new Todo({ title: 'Update Resume',   completed: false, user: sue._id });
-  return Todo.create([groceries, feedTheCat, learnAngular, updateResume]);
-})
-.then(function(savedTodos) {
-  console.log('Just saved', savedTodos.length, 'todos.');
-  return Todo.find({}).populate('user');
-})
-.then(function(allTodos) {
-  console.log('Printing all todos:');
-  allTodos.forEach(function(todo) {
-    console.log(todo.toString());
-  });
-  quit();
-}, function(err) {
+.then(function() {
+  console.log('creating some new restaurants...');
+  var fellinis    = new Restaurant({ title: 'Fellinis' });
+    console.log(fellinis);
+    Restaurant.create([fellinis]);
+    quit();
+  }),
+// .then(function(restaurant) {
+//   var test = User.find({ "local.email" : "hello@itsme.com" });
+//   console.log(test)
+//   .then(function(adele) {
+//     adele.restaurants.push(restaurant._id);
+//   })
+// }),
+// .then(function(savedTodos) {
+//   console.log('Just saved', savedTodos.length, 'todos.');
+//   return Todo.find({}).populate('user');
+// })
+// .then(function(allTodos) {
+//   console.log('Printing all todos:');
+//   allTodos.forEach(function(todo) {
+//     console.log(todo.toString());
+//   });
+ function(err) {
   return handleError(err);
-});
+}
