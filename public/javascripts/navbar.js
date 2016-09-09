60d03c17f25d3a3d048bd667edec490179229604
Leslie Hochsztein <lhochsz@gmail.com>
2016-09-09 16:54:01 -0400

    A bit of navbar styling.

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -5,7 +5,7 @@ angular.module('myApp')
       <div class="container-fluid">
         <div class="navbar-header">
           <a class="navbar-brand" ui-sref="home">
-            <span class="glyphicon glyphicon-home"></span> Order Up
+            Order Up
           </a>
         </div>
 
