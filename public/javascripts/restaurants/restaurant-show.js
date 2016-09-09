d591730078bffab847caa089e44f2b20c17a0f97
Patrick <pqrosset@gmail.com>
2016-09-09 12:47:05 -0400

    trying to get current user in show

--- public/javascripts/restaurants/restaurant-show.js
+++ public/javascripts/restaurants/restaurant-show.js
@@ -7,7 +7,7 @@ angular.module('myApp')
 
     <div class="items" ng-repeat="item in $ctrl.restaurantInfo.items">
       <a ng-click="$ctrl.show(item)">{{ item.title }}</a>
-      <button ng-click="$ctrl.delete(item)" class="btn btn-xs btn-danger">X</button>
+      <button ng-if="{{currentUser._id === $ctrl.restaurantInfo.restaurant.owner}}" ng-click="$ctrl.delete(item)" class="btn btn-xs btn-danger">X</button>
     </div>
 
     <a ui-sref="restaurants" class="btn btn-primary">Back</a>
