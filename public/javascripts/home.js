72423b1e950ccb371672c76deba3c673ae461564
Awesome <bc.lucas89@gmail.com>
2016-09-12 18:38:49 -0400

    CAROUSEL IS OPERATIONAL

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,84 +1,34 @@
 // angular.module('myApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
 angular.module('myApp')
     .component('home', {
-            template: `
-  <section class="container well text-center">
-    <h1>{{ $ctrl.name }}</h1>
-  </section>
-<div ng-controller="CarouselDemoCtrl">
-  <div style="height: 305px">
-    <div uib-carousel active="active" interval="myInterval" no-wrap="noWrapSlides">
-      <div uib-slide ng-repeat="slide in slides track by slide.id" index="slide.id">
-        <img ng-src="{{slide.image}}" style="margin:auto;">
-        <div class="carousel-caption">
-          <h4>Slide {{slide.id}}</h4>
-          <p>{{slide.text}}</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-md-6">
-      </div>
-    </div>
-    <div class="col-md-6">
-    </div>
-  </div>
-</div>
-</section>
+        template: `
+          <section class="container well text-center">
+            <h1>{{ $ctrl.name }}</h1>
+            <div>
+                <div id="slides_control" uib-carousel active="active" interval="myInterval" no-wrap="noWrapSlides">
+                   <div uib-slide ng-repeat ="slide in $ctrl.slides" active="slide.active" index="slide.id">
+                        <img ng-src="{{slide.image}}" style="margin:auto;">
+                    <div class="carousel-caption">
+                    </div>
+                   </div>
+                </div>
+            </div>
+         </section>
 
   `,
-            controller: function($scope) {
-                //this.name = 'Order Up';
-                $scope.myInterval = 5000;
-                $scope.noWrapSlides = false;
-                $scope.active = 0;
-                var slides = $scope.slides = [];
-                var currIndex = 0;
-                $scope.addSlide = function() {
-                    var newWidth = 600 + slides.length + 1;
-                    slides.push({
-                        image: '//unsplash.it/' + newWidth + '/300',
-                        text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
-                        id: currIndex++
-                    });
-                };
-                $scope.randomize = function() {
-                    var indexes = generateIndexesArray();
-                    assignNewIndexesToSlides(indexes);
-                };
-                for (var i = 0; i < 4; i++) {
-                    $scope.addSlide();
-                }
-                // Randomize logic below
-                function assignNewIndexesToSlides(indexes) {
-                    for (var i = 0, l = slides.length; i < l; i++) {
-                        slides[i].id = indexes.pop();
-                    }
-                }
-                function generateIndexesArray() {
-                    var indexes = [];
-                    for (var i = 0; i < currIndex; ++i) {
-                        indexes[i] = i;
-                    }
-                    return shuffle(indexes);
-                }
-                function shuffle(array) {
-                    var tmp, current, top = array.length;
+        controller: function() {
+            this.name = 'Order Up';
+            this.myInterval = 3000;
+            this.noWrapSlides = false;
+            this.active = 0;
+            this.slides = [
+                { image: 'http://lorempixel.com/400/200/food', text: 'this is a burger', id: 0 },
+                { image: 'http://lorempixel.com/400/200/food', text: 'akdf', id: 1 },
+                { image: 'http://lorempixel.com/400/200/food', text: 'kafjldkf', id: 2 },
+                { image: 'http://lorempixel.com/400/200/food', text: 'etajtla', id: 3 }
+            ];
 
-                    if (top) {
-                        while (--top) {
-                            current = Math.floor(Math.random() * (top + 1));
-                            tmp = array[current];
-                            array[current] = array[top];
-                            array[top] = tmp;
-                        }
-                    }
-                    return array;
-
-                }
-            }
+        }
 
 
-});
-//
+    });
