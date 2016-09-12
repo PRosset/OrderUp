fbff938f296eaee780450c4409b948f4f95f1534
Awesome <bc.lucas89@gmail.com>
2016-09-12 13:43:39 -0400

    working out some bugs

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -1,73 +1,84 @@
-
-angular.module('myApp', ['angular-carousel']);
+// angular.module('myApp', ['ngAnimate', 'ngSanitize', 'ui.bootstrap']);
+angular.module('myApp')
     .component('home', {
-template:`
-<section class="container well text-center">
+            template: `
+  <section class="container well text-center">
     <h1>{{ $ctrl.name }}</h1>
+  </section>
+<div ng-controller="CarouselDemoCtrl">
+  <div style="height: 305px">
+    <div uib-carousel active="active" interval="myInterval" no-wrap="noWrapSlides">
+      <div uib-slide ng-repeat="slide in slides track by slide.id" index="slide.id">
+        <img ng-src="{{slide.image}}" style="margin:auto;">
+        <div class="carousel-caption">
+          <h4>Slide {{slide.id}}</h4>
+          <p>{{slide.text}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-md-6">
+      </div>
+    </div>
+    <div class="col-md-6">
+    </div>
+  </div>
+</div>
 </section>
-<ul rn-carousel class="image">
-  <li ng-repeat="image in sportImages">
-    <div class="layer">{{ image }}</div>
-  </li>
-  <li ng-repeat="image in sportImages">
-    <div class="layer">https://hd.unsplash.com/photo-1424847651672-bf20a4b0982b</div>
-  </li>
-</ul>
-`,
-)};
-controller: function('CarouselDemoCtrl', function ($scope) {
-  $scope.myInterval = 5000;
-  $scope.noWrapSlides = false;
-  $scope.active = 0;
-  var slides = $scope.slides = [];
-  var currIndex = 0;
-};
-  $scope.addSlide = function() {
-    var newWidth = 600 + slides.length + 1;
-    slides.push({
-      image: '//unsplash.it/' + newWidth + '/300',
-      text: ['Nice image','Awesome photograph','That is so cool','I love that'][slides.length % 4],
-      id: currIndex++
-    });
-  };
 
-  $scope.randomize = function() {
-    var indexes = generateIndexesArray();
-    assignNewIndexesToSlides(indexes);
-  };
+  `,
+            controller: function($scope) {
+                //this.name = 'Order Up';
+                $scope.myInterval = 5000;
+                $scope.noWrapSlides = false;
+                $scope.active = 0;
+                var slides = $scope.slides = [];
+                var currIndex = 0;
+                $scope.addSlide = function() {
+                    var newWidth = 600 + slides.length + 1;
+                    slides.push({
+                        image: '//unsplash.it/' + newWidth + '/300',
+                        text: ['Nice image', 'Awesome photograph', 'That is so cool', 'I love that'][slides.length % 4],
+                        id: currIndex++
+                    });
+                };
+                $scope.randomize = function() {
+                    var indexes = generateIndexesArray();
+                    assignNewIndexesToSlides(indexes);
+                };
+                for (var i = 0; i < 4; i++) {
+                    $scope.addSlide();
+                }
+                // Randomize logic below
+                function assignNewIndexesToSlides(indexes) {
+                    for (var i = 0, l = slides.length; i < l; i++) {
+                        slides[i].id = indexes.pop();
+                    }
+                }
+                function generateIndexesArray() {
+                    var indexes = [];
+                    for (var i = 0; i < currIndex; ++i) {
+                        indexes[i] = i;
+                    }
+                    return shuffle(indexes);
+                }
+                function shuffle(array) {
+                    var tmp, current, top = array.length;
 
-  for (var i = 0; i < 4; i++) {
-    $scope.addSlide();
-  }
+                    if (top) {
+                        while (--top) {
+                            current = Math.floor(Math.random() * (top + 1));
+                            tmp = array[current];
+                            array[current] = array[top];
+                            array[top] = tmp;
+                        }
+                    }
+                    return array;
 
-  // Randomize logic below
+                }
+            }
 
-  function assignNewIndexesToSlides(indexes) {
-    for (var i = 0, l = slides.length; i < l; i++) {
-      slides[i].id = indexes.pop();
-    }
-  }
 
-  function generateIndexesArray() {
-    var indexes = [];
-    for (var i = 0; i < currIndex; ++i) {
-      indexes[i] = i;
-    }
-    return shuffle(indexes);
-  }
- // http://stackoverflow.com/questions/962802#962890
-  function shuffle(array) {
-    var tmp, current, top = array.length;
-
-    if (top) {
-      while (--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = array[current];
-        array[current] = array[top];
-        array[top] = tmp;
-      }
-    }
-
-    return array;
-  }
 });
+//
