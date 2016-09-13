455e8ad1b3132f13270a1cbb5e1eeadee6b5bb34
Awesome <bc.lucas89@gmail.com>
2016-09-13 11:14:10 -0400

    Work on Carousel to display fullscreen next is creating filepaths to photos in library

--- public/javascripts/home.js
+++ public/javascripts/home.js
@@ -2,19 +2,14 @@
 angular.module('myApp')
     .component('home', {
         template: `
-          <section class="container well text-center">
-            <h1>{{ $ctrl.name }}</h1>
-            <div>
                 <div id="slides_control" uib-carousel active="active" interval="myInterval" no-wrap="noWrapSlides">
-                   <div uib-slide ng-repeat ="slide in $ctrl.slides" active="slide.active" index="slide.id">
+                   <div uib-slide ng-repeat ="slide in $ctrl.slides" index="slide.id">
                         <img ng-src="{{slide.image}}" style="margin:auto;">
                     <div class="carousel-caption">
+                    <h3>Explore {{slide.text}}</h4>
                     </div>
                    </div>
                 </div>
-            </div>
-         </section>
-
   `,
         controller: function() {
             this.name = 'Order Up';
@@ -22,10 +17,10 @@ angular.module('myApp')
             this.noWrapSlides = false;
             this.active = 0;
             this.slides = [
-                { image: 'http://lorempixel.com/400/200/food', text: 'this is a burger', id: 0 },
-                { image: 'http://lorempixel.com/400/200/food', text: 'akdf', id: 1 },
-                { image: 'http://lorempixel.com/400/200/food', text: 'kafjldkf', id: 2 },
-                { image: 'http://lorempixel.com/400/200/food', text: 'etajtla', id: 3 }
+                { image: 'http://lorempixel.com/400/200/food', text: 'Pat\'s', id: 0},
+                { image: 'http://lorempixel.com/400/200/food', text: 'Dan\'s', id: 1},
+                { image: 'http://lorempixel.com/400/200/food', text: 'Leslie\'s', id: 2 },
+                { image: 'http://lorempixel.com/400/200/food', text: 'Brandon\'s', id: 3 }
             ];
 
         }
