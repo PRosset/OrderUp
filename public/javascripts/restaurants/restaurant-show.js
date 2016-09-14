d10e8570f9bc69bdd39d23bd68dbbd1b199a7134
Patrick <pqrosset@gmail.com>
2016-09-14 11:13:37 -0400

    fixed menu on restaurant-show.... somehow... don't ask questions

--- public/javascripts/restaurants/restaurant-show.js
+++ public/javascripts/restaurants/restaurant-show.js
@@ -2,43 +2,41 @@ angular.module('myApp')
 .component('restaurantShow', {
   template: `
   <div class="col-md-10 col-md-offset-1">
-    <br>
-      <div class="row">
-        <div class="col-md-1">
-          <a ui-sref="restaurants" class="btn btn-primary">Back</a>
-        </div>
-        <div class="col-md-3 col-md-offset-8">
-          <div class="row">
-            <a ng-if="$ctrl.checkOwner(restaurant)" ng-click="$ctrl.edit(restaurant)" class="btn btn-warning">Edit</a>
-            <a ng-if="$ctrl.checkOwner(restaurant)" ng-click="$ctrl.newItem(restaurant)" class="btn btn-warning">New Menu Item</a>
-          </div>
-        </div>
+    <br></br>
+    <div class="row">
+      <div class="col-md-1">
+        <a ui-sref="restaurants" class="btn btn-primary">Back</a>
+      </div>
+      <div class="row col-md-3 col-md-offset-8">
+        <a ng-if="$ctrl.checkOwner(restaurant)" ng-click="$ctrl.edit(restaurant)" class="btn btn-warning">Edit</a>
+        <a ng-if="$ctrl.checkOwner(restaurant)" ng-click="$ctrl.newItem(restaurant)" class="btn btn-warning">New Menu Item</a>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="restaurantDetails col-md-3">
+        <h3>{{ $ctrl.restaurantInfo.restaurant.title }}</h3>
+        <hr/>
+        <p><b>Cuisine: </b>{{ $ctrl.restaurantInfo.restaurant.cuisine }}</p>
+        <p><b>Address: </b>{{ $ctrl.restaurantInfo.restaurant.address }}</p>
+        <p><b>Phone: </b>{{ $ctrl.restaurantInfo.restaurant.phone }}</p>
+        <p><b>Hours: </b>{{ $ctrl.restaurantInfo.restaurant.hours }}</p>
       </div>
 
-      <div class="row">
-        <div class="restaurantDetails col-md-3">
-          <h3>{{ $ctrl.restaurantInfo.restaurant.title }}</h3>
-          <hr/>
-          <p><b>Cuisine: </b>{{ $ctrl.restaurantInfo.restaurant.cuisine }}</p>
-          <p><b>Address: </b>{{ $ctrl.restaurantInfo.restaurant.address }}</p>
-          <p><b>Phone: </b>{{ $ctrl.restaurantInfo.restaurant.phone }}</p>
-          <p><b>Hours: </b>{{ $ctrl.restaurantInfo.restaurant.hours }}</p>
-        </div>
-
-        <div class="menuItems col-md-8 col-md-offset-1">
-          <div class="categories">
-            <div class="row">
-              <div class="col-md-10 col-md-offset-1">
-                <uib-tabset type="pills" active="activeItemId" ng-model="$ctrl.item.category">
-                  <uib-tab class="pillButton" ng-repeat="category in $ctrl.categories" heading="{{category.title}}" ng-click="$ctrl.sendCategory(category.searchParam)"></uib-tab>
-                </uib-tabset>
-              </div>
+      <div class="menuItems col-md-8 col-md-offset-1">
+        <div class="categories">
+          <div class="row">
+            <div class="col-md-10 col-md-offset-1">
+              <uib-tabset type="pills" active="activeItemId" ng-model="$ctrl.item.category">
+                <uib-tab class="pillButton" ng-repeat="category in $ctrl.categories" heading="{{category.title}}" ng-click="$ctrl.sendCategory(category.searchParam)"></uib-tab>
+              </uib-tabset>
             </div>
           </div>
         </div>
         <div class="scrollMenu">
           <div class="items" ng-repeat="item in $ctrl.restaurantInfo.items | orderBy: 'category' | filter : $ctrl.search ">
-            <h4><a class="itemTitle" ng-click="$ctrl.show(item)">{{ item.title }}</a>
+            <h4><a class="itemTitle" ng-if="$ctrl.checkOwner(restaurant)" ng-click="$ctrl.show(item)">{{ item.title }}</a>
+            <h4><p class="itemTitle" ng-if="!$ctrl.checkOwner(restaurant)">{{ item.title }}</p>
               <button ng-if="$ctrl.checkOwner(restaurant)" ng-click="$ctrl.deleteItem(item)" class="btn btn-xs btn-danger">X</button>
             </h4>
             <p class="itemDescription">{{ item.description }}</p>
@@ -46,7 +44,8 @@ angular.module('myApp')
           </div>
         </div>
       </div>
-  </div
+    </div>
+  </div>
   `,
 
   controller: function(restaurantService, Auth, $state, $stateParams) {
@@ -57,11 +56,14 @@ angular.module('myApp')
 
     this.sendCategory = function(category) {
      this.search = category;
+     console.log(category.id);
+     console.log('I fired');
+     console.log(this.search);
     };
 
 
     this.show = function(item) {
-      $state.go('item-show', { id: item._id});
+      $state.go('item-show', { id: item._idn});
     };
 
     this.edit = function() {
@@ -89,3 +91,10 @@ angular.module('myApp')
   }
 });
 
+
+
+    // <div class="categories col-xs-6 col-md-offset-3">
+    //   <uib-tabset type="pills" active="activeItemId" ng-model="$ctrl.item.category" ng-repeat="category in $ctrl.categories">
+    //     <uib-tab heading="{{category}}" ng-click="$ctrl.sendCategory(category)"></uib-tab>
+    //   </uib-tabset>
+    // </div>
