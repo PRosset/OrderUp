31df145ab7614d58a7ce954dc1e0099dfb201578
Leslie Hochsztein <lhochsz@gmail.com>
2016-09-14 12:13:08 -0400

    Navbar now responsive with hamburger menu

--- public/javascripts/navbar.js
+++ public/javascripts/navbar.js
@@ -4,10 +4,15 @@ angular.module('myApp')
     <nav class="navbar navbar-fixed-top navbar-default">
       <div class="container-fluid">
         <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
           <p class="navbar-brand orderUp">Order Up</p>
         </div>
-        </div>
-        <div>
+      </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
           <ul class="nav navbar-nav">
             <li ng-class="{ active: $ctrl.$state.includes('home') }" ><a ui-sref="home">Home</a></li>
             <li ng-class="{ active: $ctrl.$state.includes('about') }" ><a ui-sref="about">About</a></li>
